16d59c6be95b78cd9c31223a25151d8231d3f336
Franco Lombardo <f.lombardo69@gmail.com>
2021-03-12 16:11:33 +0100

    Test for Cert method

--- spidsaml/spid_test.go
+++ spidsaml/spid_test.go
@@ -2,6 +2,7 @@ package spidsaml
 
 import (
 	"crypto/rsa"
+	"crypto/x509"
 	"testing"
 )
 
@@ -40,31 +41,52 @@ func TestSP_Key(t *testing.T) {
 	}
 }
 
+func TestSP_Cert(t *testing.T) {
+	cases := []struct {
+		certFile  string
+		returnErr bool
+		name      string
+	}{
+		{
+			certFile:  "non_existing_file.pem",
+			returnErr: true,
+			name:      "Gives error when certificate file does not exist",
+		},
+		{
+			certFile:  "../fixtures/crt.pem",
+			returnErr: false,
+			name:      "Can read a certificate file",
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			_, err := readCert(tc.certFile)
+			returnedErr := err != nil
+
+			if returnedErr != tc.returnErr {
+				t.Fail()
+			}
+		})
+	}
+}
+
+func readCert(certFile string) (key *x509.Certificate, err interface{}) {
+	defer func() {
+		err = recover()
+	}()
+	sp := &SP {
+		CertFile: certFile,
+	}
+	return sp.Cert(), nil
+}
+
 func readKey(keyFile string) (key *rsa.PrivateKey, err interface{}) {
 	defer func() {
 		err = recover()
 	}()
-	sp := createSPWith(keyFile)
-	return sp.Key(), nil
-}
-
-func createSPWith(keyFile string) *SP {
-	sp := &SP{
-		EntityID: "https://spid.comune.roma.it",
-		KeyFile:  keyFile,
-		CertFile: "../fixtures/crt.pem",
-		AssertionConsumerServices: []string{
-			"http://localhost:8000/spid-sso",
-		},
-		SingleLogoutServices: map[string]SAMLBinding{
-			"http://localhost:8000/spid-slo": HTTPRedirect,
-		},
-		AttributeConsumingServices: []AttributeConsumingService{
-			{
-				ServiceName: "Service 1",
-				Attributes:  []string{"fiscalNumber", "name", "familyName", "dateOfBirth"},
-			},
-		},
+	sp := &SP {
+		KeyFile: keyFile,
 	}
-	return sp
+	return sp.Key(), nil
 }
