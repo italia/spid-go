17f9cec926f4b010b22e3b9d8639842bae648ee6
Alessandro Ranellucci <alranel@teamdigitale.governo.it>
2024-06-03 10:49:26 +0200

    Move Clock to messages.go

--- spidsaml/messages.go
+++ spidsaml/messages.go
@@ -1,5 +1,20 @@
 package spidsaml
 
+import "time"
+
+// Clock is a clock that can be mocked in tests. Thanks to Michael Whatcott
+// See https://smartystreets.com/blog/2015/09/go-testing-part-5-testing-with-time/
+type Clock struct {
+	instant time.Time
+}
+
+func (this *Clock) Now() time.Time {
+	if this == nil {
+		return time.Now()
+	}
+	return this.instant
+}
+
 // protocolMessage is the base class for all SAML messages
 type protocolMessage struct {
 	SP    *SP
--- spidsaml/spid.go
+++ spidsaml/spid.go
@@ -1,7 +1,5 @@
 package spidsaml
 
-import "time"
-
 // Session represents an active SPID session.
 type Session struct {
 	IDPEntityID  string
@@ -10,17 +8,4 @@ type Session struct {
 	AssertionXML []byte
 	Level        int
 	Attributes   map[string]string
-}
-
-// Clock is a clock that can be mocked in tests. Thanks to Michael Whatcott
-// See https://smartystreets.com/blog/2015/09/go-testing-part-5-testing-with-time/
-type Clock struct {
-	instant time.Time
-}
-
-func (this *Clock) Now() time.Time {
-	if this == nil {
-		return time.Now()
-	}
-	return this.instant
 }
